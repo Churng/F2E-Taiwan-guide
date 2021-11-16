ea4bf4b314865791f665569e0309947a08b0b527
Churng <churng0421@gmail.com>
2021-11-16 10:07:28 +0800

    Footer建置

--- share/Footer.js
+++ share/Footer.js
@@ -1,20 +1,125 @@
 export default function footer({ children }) {
 	return (
 		<>
-			<footer className="text-muted py-5">
-				<div className="container">
-					<p className="float-end mb-1">
-						<a href="#">Back to top</a>
-					</p>
-					<p className="mb-1">
-						Album example is &copy; Bootstrap, but please download and customize it for yourself!
-					</p>
-					<p className="mb-0">
-						New to Bootstrap? Visit the homepageor read our{" "}
-						<a href="/docs/5.1/getting-started/introduction/">getting started guide</a>.
-					</p>
+			<div className="container-fluid footer">
+				<footer className="row row-cols-5 py-5 my-5 border-top ">
+					<div className="col">
+						<p className="text-muted">© 2021</p>
+					</div>
+
+					<div className="col"></div>
+
+					<div className="col">
+						<h5>Section</h5>
+						<ul className="nav flex-column">
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Home
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Features
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Pricing
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									FAQs
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									About
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div className="col">
+						<h5>Section</h5>
+						<ul className="nav flex-column">
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Home
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Features
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Pricing
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									FAQs
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									About
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div className="col">
+						<h5>Section</h5>
+						<ul className="nav flex-column">
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Home
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Features
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									Pricing
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									FAQs
+								</a>
+							</li>
+							<li className="nav-item mb-2">
+								<a href="#" className="nav-link p-0 text-muted">
+									About
+								</a>
+							</li>
+						</ul>
+					</div>
+				</footer>
+				<div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+					<div className="col-md-4 d-flex align-items-center">
+						<span className="text-muted">© 2021 Company, Inc</span>
+					</div>
+
+					<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+						<li className="ms-3">
+							<a className="text-muted" href="#"></a>
+						</li>
+						<li className="ms-3">
+							<a className="text-muted" href="#"></a>
+						</li>
+						<li className="ms-3">
+							<a className="text-muted" href="#"></a>
+						</li>
+					</ul>
 				</div>
-			</footer>
+			</div>
 		</>
 	);
 }
